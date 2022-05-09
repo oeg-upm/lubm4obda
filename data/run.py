158e028a1cb7430bf76b0138cf9d70e6ee7c11ce
Julián Arenas Guerrero <arenas.guerrero.julian@outlook.com>
2022-05-09 11:47:38 +0200

    update undergraduatedegreeyear

--- data/run.py
+++ data/run.py
@@ -83,9 +83,23 @@ os.system(f"cat /temp/Universities-1.sql >> {output_path}{output_file_name}")
 
 if r2 == 'SQL':
     os.system(f"echo 'ALTER TABLE graduateStudent CHANGE age undergraduateDegreeYear int;' >> {output_path}{output_file_name}")
+    os.system(f"echo 'UPDATE graduateStudent set undergraduateDegreeYear=\"2010\" where undergraduateDegreeYear=20;' >> {output_path}{output_file_name}")
+    os.system(f"echo 'UPDATE graduateStudent set undergraduateDegreeYear=\"2011\" where undergraduateDegreeYear=21;' >> {output_path}{output_file_name}")
+    os.system(f"echo 'UPDATE graduateStudent set undergraduateDegreeYear=\"2012\" where undergraduateDegreeYear=22;' >> {output_path}{output_file_name}")
+    os.system(f"echo 'UPDATE graduateStudent set undergraduateDegreeYear=\"2013\" where undergraduateDegreeYear=23;' >> {output_path}{output_file_name}")
+    os.system(f"echo 'UPDATE graduateStudent set undergraduateDegreeYear=\"2014\" where undergraduateDegreeYear=24;' >> {output_path}{output_file_name}")
+    os.system(f"echo 'UPDATE graduateStudent set undergraduateDegreeYear=\"2015\" where undergraduateDegreeYear=25;' >> {output_path}{output_file_name}")
+    os.system(f"echo 'UPDATE graduateStudent set undergraduateDegreeYear=\"2016\" where undergraduateDegreeYear=26;' >> {output_path}{output_file_name}")
     os.system(f"echo 'UPDATE graduateStudent set undergraduateDegreeYear=\"2017\" where undergraduateDegreeYear=27;' >> {output_path}{output_file_name}")
 elif r2 == 'PostgreSQL':
     os.system(f"echo 'ALTER TABLE graduatestudent RENAME COLUMN age TO undergraduatedegreeyear;' >> {output_path}{output_file_name}")
+    os.system(f"echo 'UPDATE graduatestudent set undergraduatedegreeyear=2010 where undergraduatedegreeyear=20;' >> {output_path}{output_file_name}")
+    os.system(f"echo 'UPDATE graduatestudent set undergraduatedegreeyear=2011 where undergraduatedegreeyear=21;' >> {output_path}{output_file_name}")
+    os.system(f"echo 'UPDATE graduatestudent set undergraduatedegreeyear=2012 where undergraduatedegreeyear=22;' >> {output_path}{output_file_name}")
+    os.system(f"echo 'UPDATE graduatestudent set undergraduatedegreeyear=2013 where undergraduatedegreeyear=23;' >> {output_path}{output_file_name}")
+    os.system(f"echo 'UPDATE graduatestudent set undergraduatedegreeyear=2014 where undergraduatedegreeyear=24;' >> {output_path}{output_file_name}")
+    os.system(f"echo 'UPDATE graduatestudent set undergraduatedegreeyear=2015 where undergraduatedegreeyear=25;' >> {output_path}{output_file_name}")
+    os.system(f"echo 'UPDATE graduatestudent set undergraduatedegreeyear=2016 where undergraduatedegreeyear=26;' >> {output_path}{output_file_name}")
     os.system(f"echo 'UPDATE graduatestudent set undergraduatedegreeyear=2017 where undergraduatedegreeyear=27;' >> {output_path}{output_file_name}")
 
 
